6aa94356db6aff1afb6d7648d5dbe47451ae04e8
GABRIELA MOTTA OLIVEIRA 114110443 <gabrielako@lcc3-064.lcc.ufcg.edu.br>
2018-09-05 17:26:34 -0300

    update book model :green_book:

--- models/Book.js
+++ models/Book.js
@@ -2,7 +2,32 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
-    name: String
+    title: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    sinopsis: {
+      type: String,
+      required: false,
+      default: "No information now, come back soon."
+    },
+    release_date: {
+      type: Date,
+      required: false
+    },
+    original_language: {
+      type: String,
+      required: false
+    },
+    quotes: {
+      type: [String],
+      default: [],
+      required: false
+    }
 });
 
 var Book = mongoose.model('Book', BookSchema);
