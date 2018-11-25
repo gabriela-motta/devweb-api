08277c8e2ad3c71c490a751de4982bf74a7f4239
Gabriela <gabriela.oliveira@ccc.ufcg.edu.br>
2018-11-24 22:11:58 -0300

    add db url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "",
+    url: "mongodb://"+process.env.DATABASE_USERNAME+":"+process.env.DATABASE_PASSWORD+"@ds151382.mlab.com:51382/kitso-books",
     local_url: "mongodb://localhost:27017/kitsobooks-dev"
 }
