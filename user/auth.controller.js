0280d68b66c67998b4c70208b9351561ec441364
Gabriela Motta Oliveira <gabriela.oliveira@ccc.ufcg.edu.br>
2018-12-14 00:43:42 -0300

    Update auth.controller.js

--- user/auth.controller.js
+++ user/auth.controller.js
@@ -4,28 +4,19 @@ var RequestStatus = require('../constants/requestStatus');
 
 exports.login = function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      return next(err);
-    }
-
-    if (!user) {
-      return res.status(RequestStatus.UNAUTHORIZED).json({ err: info });
-    }
-
+    if (err) return next(err);
+    if (!user) return res.status(RequestStatus.UNAUTHORIZED).json({ err: info });
     req.logIn(user, function(err) {
-      if (err) {
-        return res.status(RequestStatus.INTERNAL_SERVER_ERROR).json({ err: 'Could not log in user' });
-      }
-
-      res.status(RequestStatus.OK).json({ status: 'Login successful!' });
-
+      console.log(req.sessionID)
+      if (err) return res.status(RequestStatus.INTERNAL_SERVER_ERROR).json({ err: 'Could not log in user' });
+      res.status(RequestStatus.OK).json({ status: 'Login successful!', session: req.sessionID });
     });
   })(req, res, next);
 }
 
 exports.logout = function(req, res) {
   req.logout();
-  res.status(RequestStatus.OK).send('Logged out!')
+  res.status(RequestStatus.OK).send({status:'Logged out!'})
 }
 
 exports.status = function(req, res) {
